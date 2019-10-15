8886da32b02eccad89b0a40d82c8003aec1b7421
quiquegrases <enric.grases@socialpoint.es>
2019-10-15 11:30:16 +0200

    First commit

---
+++ Assets/Art/Scripts_Art/AI/UnitMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public delegate void UnitHasStopped(UnitMovement unit);
+
+public class UnitMovement : MonoBehaviour
+{
+    public event UnitHasStopped UnitHasStoppedRequest;
+    private const float REMAINING_DISTANCE = 2f;
+    private NavMeshAgent _navMeshAgent;
+
+    private void Awake()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    public void SetDestination(Vector3 destination)
+    {
+        if (_navMeshAgent == null)
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+
+        _navMeshAgent.SetDestination(destination);
+    }
+
+    public bool UnitHasStopped()
+    {
+        return _navMeshAgent.remainingDistance > REMAINING_DISTANCE ? false : true;
+    }
+}
